dad8af08cbb6e79fce1cf60faa583ca5c70dedcc
Soumi Kaibartya <64211618+Soumi-Kaibartya@users.noreply.github.com>
2021-05-14 13:34:29 +0530

    Add files via upload

--- hello1.py
+++ hello1.py
@@ -1,1 +1,11 @@
-
+mondaytemp = [1,4,5]
+print(mondaytemp)
+mondaytemp.append(6)
+print(mondaytemp)
+a = [8,2]
+mondaytemp.append(a)
+print(mondaytemp)
+mondaytemp.extend([4,6,8,10])
+print(mondaytemp)
+mondaytemp.insert(4,99)
+print(mondaytemp)
